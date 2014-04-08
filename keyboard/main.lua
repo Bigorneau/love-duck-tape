0d9187b3bdd4a0b9a172493b71bacebff451b69d
Patrice Blin <dev@patriceblin.com>
2014-04-08 20:00:24 +0200

    key: simple move point

---
+++ keyboard/main.lua
@@ -0,0 +1,21 @@
+local width = love.graphics.getWidth()
+local height = love.graphics.getHeight()
+local point = {width / 2, height / 2}
+
+function love.draw()
+	love.graphics.point(point[1], point[2])
+end
+
+function love.keypressed(key)
+	if key == "left" then
+		point[1] = (point[1] - 10) % width
+	elseif key == "right" then
+		point[1] = (point[1] + 10) % width
+	elseif key == "up" then
+		point[2] = (point[2] - 10) % height
+	elseif key == "down" then
+		point[2] = (point[2] + 10) % height
+	else
+		print(key)
+	end
+end
