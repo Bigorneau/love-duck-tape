3242a2d0cdba7e7706c314b4ee2e60c91b11fca9
Patrice Blin <dev@patriceblin.com>
2014-04-08 21:04:34 +0200

    key: combo key and faster

--- keyboard/main.lua
+++ keyboard/main.lua
@@ -10,16 +10,23 @@ function love.draw()
 	love.graphics.point(point[1], point[2])
 end
 
-function love.keypressed(key)
-	if key == "left" then
-		point[1] = (point[1] - 10) % width
-	elseif key == "right" then
-		point[1] = (point[1] + 10) % width
-	elseif key == "up" then
-		point[2] = (point[2] - 10) % height
-	elseif key == "down" then
-		point[2] = (point[2] + 10) % height
-	else
-		print(key)
+function love.update(dt)
+	local x = point[1]
+	local y = point[2]
+	if love.keyboard.isDown("left") then
+		x = x - 10
 	end
+	if love.keyboard.isDown("right") then
+		x = x + 10
+	end
+	if love.keyboard.isDown("up") then
+		y = y - 10
+	end
+	if love.keyboard.isDown("down") then
+		y = y + 10
+	end
+
+	-- bounds
+	point[1] = x % width
+	point[2] = y % height
 end
